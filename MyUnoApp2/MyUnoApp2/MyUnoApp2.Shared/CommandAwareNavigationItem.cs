aa3a9377826de7757e9f76aa27e8c5ec4478bb06
Julie Cantin <Julie.cantin@nventive.com>
2022-07-21 13:12:02 -0400

    sample to repro nav view

---
+++ MyUnoApp2/MyUnoApp2/MyUnoApp2.Shared/CommandAwareNavigationItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace MyUnoApp2
+{
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("nventive.Input", "NV0117:NV0117 - RegisterAttribute is required on NSObject derived classes", Justification = "Not really required")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("nventive.Reliability", "NV0126:NV0126 - Classes inheriting from UIKit.UIView or Android.Views.View must implement a native contructor", Justification = "Not really required")]
+    public partial class CommandAwareNavigationViewItem : NavigationViewItem
+    {
+        public ICommand NavigationCommand
+        {
+            get { return (ICommand)GetValue(NavigationCommandProperty); }
+            set { SetValue(NavigationCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty NavigationCommandProperty =
+            DependencyProperty.Register(nameof(NavigationCommand), typeof(ICommand), typeof(CommandAwareNavigationViewItem), new PropertyMetadata(default(ICommand)));
+    }
+}
---
+++ MyUnoApp2/MyUnoApp2/MyUnoApp2.Shared/NavigationViewItemTemplateSelector.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace MyUnoApp2
+{
+	public class NavigationViewItemTemplateSelector : DataTemplateSelector
+	{
+		public DataTemplate OverviewTemplate { get; set; }
+
+		public DataTemplate MessagingTemplate { get; set; }
+
+		public DataTemplate StatementsAndDocumentsTemplate { get; set; }
+
+		public DataTemplate FinancialPlanTemplate { get; set; }
+
+		public DataTemplate NetWorthTemplate { get; set; }
+
+		public DataTemplate GoalsTemplate { get; set; }
+
+		public DataTemplate ResourcesTemplate { get; set; }
+
+		public DataTemplate ChatTemplate { get; set; }
+
+		public DataTemplate SeparatorTemplate { get; set; }
+
+		public DataTemplate MobileWebsiteTemplate { get; set; }
+
+		protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+		{
+			return SelectTemplateCore(item);
+		}
+
+		[SuppressMessage("High cyclomatic complexity", "NV2005", Justification = "Nothing complex here")]
+		protected override DataTemplate SelectTemplateCore(object item)
+		{
+			switch (item?.ToString())
+			{
+				case "1":
+					return OverviewTemplate;
+				case "2":
+					return MessagingTemplate;
+				case "3":
+					return StatementsAndDocumentsTemplate;
+				case "4":
+					return FinancialPlanTemplate;
+				case "5":
+					return NetWorthTemplate;
+				case "6":
+					return GoalsTemplate;
+				case "7":
+					return ResourcesTemplate;
+				case "8":
+					return ChatTemplate;
+				case "9":
+					return SeparatorTemplate;
+				case "0":
+					return MobileWebsiteTemplate;
+				default:
+					return null;
+			}
+		}
+	}
+}
